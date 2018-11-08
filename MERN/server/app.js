116bcf683562f31e98422714653d511899e4c2bc
ankitjena <ankitjena13@gmail.com>
2018-11-08 15:38:53 +0530

    packages added to package.json and readme updated

--- MERN/server/app.js
+++ MERN/server/app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/{your app name}', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Essential boilerplates and frameworks during Hackathons
 
 ## MERN
 MERN boilerplate using create-react-app and express-generator
+Do configure your database in server/app.js before running the server
